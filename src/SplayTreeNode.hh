b3fe386e2b6c457fb16c3702bca3c7fd937d9d8e
Gagarine Yaikhom <blog.yaikhom@gmail.com>
2014-05-10 18:03:12 +0100

    Make tree a friend of node.

--- src/SplayTreeNode.hh
+++ src/SplayTreeNode.hh
@@ -1,10 +1,16 @@
+/* Copyright 2014 Gagarine Yaikhom (MIT License) */
+
 class SplayTreeNode {
 public:
-    int key;
-    SplayTreeNode *parent, *left, *right;
     SplayTreeNode(int keyValue) : key(keyValue) {
         parent = left = right = 0;
     };
     bool operator<(const SplayTreeNode& other);
     bool operator==(const SplayTreeNode& other);
+
+private:
+    int key;
+    SplayTreeNode *parent, *left, *right;
+
+    friend class SplayTree;
 };
